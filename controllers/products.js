495855f34ee023c92eb072d27c54ab2bcb5086a7
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2023-10-01 08:25:12 +0300

    SQL start

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+const ProductModel = require('../models/product');
+
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -11,17 +12,21 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new ProductModel(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  const products = ProductModel.fetchAll(productsData => {
+    res.render('shop', {
+      prods: productsData,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: productsData.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
+
 };
